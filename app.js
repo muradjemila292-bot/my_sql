af4173a73f7ccdf7d3a2a4c673c29c4f9d142d27
Jemila Murad <muradjemila292@gmail.com>
2026-01-04 20:04:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+import express from "express";
+import dotenv from "dotenv";
+import mysql from "mysql2";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/books", (req, res) => {
+  const sql =
+    "SELECT b.book_id,b.title,b.genre,b.published_year,b.is_available,a.name AS author_name FROM books b INNER JOIN authors a ON b.author_id = a.author_id";
+  pool.query(sql, (err, result) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(500).send("Database query failed");
+    }
+    res.status(200).json(result);
+  });
+});
+app.get("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const sql =
+    "SELECT * FROM books INNER JOIN authors ON books.author_id = authors.author_id WHERE books.book_id = ?";
+
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send("database query failed");
+    }
+    if (result.length == 0) {
+      return res.status(404).send(`There is no book with id ${id}`);
+    }
+    res.status(200).json(result);
+  });
+});
+app.post("/books", (req, res) => {
+  const { title, genre, published_year, is_available, author_id } = req.body;
+  const sql =
+    "INSERT INTO books (title, genre, published_year, is_available,author_id) VALUES (?,?,?,?,?)";
+  pool.query(
+    sql,
+    [title, genre, published_year, is_available, author_id],
+    (err, result) => {
+      if (err) {
+        console.log(err.message);
+        return res.status(500).send("database query failed");
+      }
+      res.status(201).send(`The book with id ${result.insertId} created`);
+    }
+  );
+});
+
+app.put("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const newInfo = req.body;
+  const checkSql = "SELECT book_id FROM books WHERE book_id = ?";
+
+  pool.query(checkSql, [id], (err, result) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(500).send("Database query failed");
+    }
+    if (result.length == 0) {
+      return res.status(404).send(`There is no book with id ${formatedId}`);
+    } else {
+      const updateSql = "UPDATE books SET ? WHERE book_id = ?";
+      pool.query(updateSql, [newInfo, id], (err, result) => {
+        if (err) {
+          console.log(err.message);
+          return res.status(500).send("Database query failed");
+        }
+        res.status(200).send(`The book with id: ${id} updated`);
+      });
+    }
+  });
+});
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const checkSql = "SELECT book_id FROM books WHERE book_id = ?";
+
+  pool.query(checkSql, [id], (err, result) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(500).send("Database query failed");
+    }
+    if (result.length == 0) {
+      return res.status(404).send(`There is no book with id ${formatedId}`);
+    } else {
+      const updateSql = "DELETE FROM books WHERE book_id = ?";
+      pool.query(updateSql, [id], (err, result) => {
+        if (err) {
+          console.log(err.message);
+          return res.status(500).send("Database query failed");
+        }
+        res.status(200).send(`The book with id: ${id} deleted`);
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
---
+++ data.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE library_db;
+USE library_db
+
+CREATE TABLE authors (
+author_id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+bio TEXT,
+country VARCHAR(50)
+)
+
+CREATE TABLE books (
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(100) NOT NULL,
+genre VARCHAR(50),
+published_year INT,
+is_available BOOLEAN DEFAULT TRUE,
+author_id INT,
+FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+)
+
+DESCRIBE authors;
+
+DESCRIBE books;
+
+INSERT INTO authors (name, bio, country) VALUES ("Alex Michaelides","British-Cypriot author,screenwriter, and novelist", "UK"),("Harper Lee", 'American novelist known for addressing racial issues',"USA"),("George Orwell","English novelist, essayist,and critic","UK"),("Jane Austen","English novelist known for her romantic fiction","UK"),("Paulo Coelho","Brazilianauthor known for philosophicalnovels","Brazil"),("Yuval Noah Harari","Israeli historian and professor, writes on humanity",'Israel');
+
+SELECT * FROM authors;
+
+INSERT INTO books (title, genre, published_year, is_available,author_id) VALUES("The Silent Patient","Thriller",2019,TRUE,1),("To Kill a Mockingbird","Fiction",1960,TRUE,2),("1984","Dystopian",1949,FALSE,3),("Pride and Prejudice","Romance",1813,TRUE,4),("The Alchemmist","Adventure",1988,TRUE,5),("Sapiens","Non-fiction",2011,TRUE,6) 
+
+SELECT * FROM books;
+
+
+
